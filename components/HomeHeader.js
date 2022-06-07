41b5e44ffab1349bbe12dc7cff9f0c9ec51ede27
Hafidz25 <hafidzalif80@gmail.com>
2022-06-07 12:47:48 +0700

    first commit

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -52,7 +52,7 @@ const HomeHeader = ({ onSearch }) => {
             color: COLORS.white,
           }}
         >
-          Hello Victoria 👋
+          Hello Hafidz 👋
         </Text>
 
         <Text
